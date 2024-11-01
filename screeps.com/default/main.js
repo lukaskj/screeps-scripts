99985adb82e36db3e225458bd5649dd5cd7279b7
Lukas K Jacomin <lukasjacomin@gmail.com>
2024-11-01 10:13:21 -0300

    chore

--- screeps.com/default/main.js
+++ screeps.com/default/main.js
@@ -8,7 +8,17 @@ module.exports.loop = function () {
 
   UI.update();
   SpawnController.update();
-
+  // switch (Game.time % 3) {
+  //   case 0:
+  //     Logger.error("Worked");
+  //     break;
+  //   case 1:
+  //     Logger.warn("Worked");
+  //     break;
+  //   case 2:
+  //     Logger.info("Worked");
+  //     break;
+  // }
   for (const name in Game.creeps) {
     const creep = Game.creeps[name];
     CreepController.from(creep).run();
