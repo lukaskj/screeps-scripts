5008283d3276be0a541e9bcd55d376b90593e7ce
Lukas K Jacomin <lukasjacomin@gmail.com>
2024-11-01 01:23:35 -0300

    chore: removed comments

--- screeps.com/default/main.js
+++ screeps.com/default/main.js
@@ -3,14 +3,12 @@ const {Logger} = require("./logger");
 const SpawnController = require("./spawn.controller");
 const UI = require("./ui");
 
-// Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], 'Harvester enis', {memory: {role: 'harvester'}});
-// Game.creeps['Harvester enis'].suicide()
 module.exports.loop = function () {
   CreepController.cleanMemory();
 
   UI.update();
   SpawnController.update();
-  // Logger.info("Logging", Game.time);
+
   for (const name in Game.creeps) {
     const creep = Game.creeps[name];
     CreepController.from(creep).run();
